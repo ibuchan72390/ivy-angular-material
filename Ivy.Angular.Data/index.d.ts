d990839415534be9fe2c9c31091d812fb38a7b69
Ian Buchan <ibuchan72390@gmail.com>
2018-03-16 10:09:32 -0700

    Relocate the Auth0 and Data projects accordingly.

--- Ivy.Angular.Data/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { BaseEntity } from './src/Models/base-entity.model';
-export { BaseEntityWithTypedId } from './src/Models/base-entity-with-typed-id.model';
-export { BaseEnumEntity } from './src/Models/base-enum-entity.model';
--- Ivy.Angular.Data/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var base_entity_model_1 = require("./src/Models/base-entity.model");
-exports.BaseEntity = base_entity_model_1.BaseEntity;
-var base_entity_with_typed_id_model_1 = require("./src/Models/base-entity-with-typed-id.model");
-exports.BaseEntityWithTypedId = base_entity_with_typed_id_model_1.BaseEntityWithTypedId;
-var base_enum_entity_model_1 = require("./src/Models/base-enum-entity.model");
-exports.BaseEnumEntity = base_enum_entity_model_1.BaseEnumEntity;
-//# sourceMappingURL=index.js.map

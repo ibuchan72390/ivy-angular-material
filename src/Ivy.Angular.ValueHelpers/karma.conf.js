94fe501023bafc5d8358174217bae788acf09321
Ian Buchan <ibuchan72390@gmail.com>
2018-03-16 10:12:43 -0700

    Seems these files weren't in source control, that's a huge issue for test configurations.

---
+++ src/Ivy.Angular.ValueHelpers/karma.conf.js
@@ -0,0 +1,78 @@
+// Karma configuration
+// Generated on Wed Mar 14 2018 15:19:36 GMT-0700 (US Mountain Standard Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'karma-typescript'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        'src/**/*.service.ts',
+        'test/*.spec.ts',
+        { pattern: 'node_modules/zone.js/dist/zone.js', included: true, watched: true }
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+        'src/**/*.service.ts': ['karma-typescript'],
+        'test/*.spec.ts': ['karma-typescript']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'karma-typescript'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome', 'Firefox'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+
+    karmaTypescriptConfig: {
+        tsconfig: './tsconfig.test.json',
+    }
+  })
+}

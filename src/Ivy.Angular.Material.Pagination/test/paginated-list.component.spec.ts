18af97e467e897a8edcbff78b2fddaf98c417411
Ian Buchan <ibuchan72390@gmail.com>
2018-03-21 08:31:19 -0700

    About to try to transition this spec to use Karma-Webpack instead of Karma-Typescript, it seems that karama-typescript is not capable of loading in all of the separate file types correctly.

--- src/Ivy.Angular.Material.Pagination/test/paginated-list.component.spec.ts
+++ src/Ivy.Angular.Material.Pagination/test/paginated-list.component.spec.ts
@@ -1,1 +1,85 @@
-﻿
+﻿import 'jasmine';
+
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule, MatOptionModule, MatInputModule, MatIconModule, MatToolbarModule, MatCardModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { configureWebTests } from 'ivy.angular.test-helpers';
+
+import { PaginatedRequest } from 'ivy.angular.data';
+
+import { PaginatedListComponent } from '../src/Components/PaginatedList/paginated-list.component';
+
+
+describe('PaginatedListComponent', () => {
+
+    // Variables & Constants
+    let fixture: ComponentFixture<PaginatedListComponent>;
+    let sut: PaginatedListComponent;
+
+
+    // SetUp
+    beforeEach(async () => {
+
+        const configureTestBed: (tb: TestBed) => void =
+            (tb: TestBed) => {
+                tb.configureTestingModule({
+                    imports: [
+                        CommonModule,
+                        FormsModule,
+
+                        MatSelectModule,
+                        MatOptionModule,
+                        MatInputModule,
+                        MatIconModule,
+                        MatToolbarModule,
+                        MatCardModule,
+
+                        FlexLayoutModule
+                    ],
+                    declarations: [
+                        PaginatedListComponent
+                    ],
+                });
+            };
+
+        await configureWebTests(configureTestBed);
+
+        fixture = TestBed.createComponent(PaginatedListComponent);
+        sut = fixture.componentInstance;
+    });
+
+
+    // canMoveNext
+    
+
+    // canMovePrevious
+    it('canMovePrevious returns true if request.pageNumber > 1', () => {
+
+        let req = new PaginatedRequest();
+
+        req.pageNumber = 10;
+        sut.request = req;
+
+        let result = sut.canMoveNext();
+
+        expect(result).toBeFalsy();
+    });
+
+    //it('canMovePrevious returns true if request.pageNumber == 0', () => {
+
+    //    let req = new PaginatedRequest();
+
+    //    req.pageNumber = 0;
+    //    sut.request = req;
+
+    //    let result = sut.canMoveNext();
+
+    //    expect(result).toBeTruthy();
+    //});
+
+});

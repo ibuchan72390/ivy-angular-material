8eafa5951ad3eeca7ceed179f67ad77f24992025
Ian Buchan <ibuchan72390@gmail.com>
2018-03-23 13:35:56 -0700

    Improve our browsers pattern here with our commented out groups.  Should eventually switch these to be yarn commands.

--- src/Ivy.Angular.Material.Helpers/karma.conf.js
+++ src/Ivy.Angular.Material.Helpers/karma.conf.js
@@ -58,7 +58,9 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    //browsers: ['Chrome', 'Firefox', 'PhantomJS'],     // Local Testing
+    //browsers: ['Chrome'],                             // Debugging
+    browsers: ['PhantomJS'],                            // Committed for CI
 
 
     // Continuous Integration mode

ddb7c583010daa6f01fe0a150fe85160325445d6
Ian Buchan <ibuchan72390@gmail.com>
2018-03-27 14:04:46 -0700

    Setup the FullImage project for refactoring.

---
+++ src/Ivy.Angular.FullImage/index.ts
@@ -0,0 +1,15 @@
+// Module
+export { IvyAngularFullImageModule } from './ivy.angular.full-image.module';
+
+
+
+// Components
+
+
+
+// Services
+
+
+
+
+// Pipes
---
+++ src/Ivy.Angular.FullImage/ivy.angular.full-image.module.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+// Angular
+import { NgModule, ModuleWithProviders } from '@angular/core';
+
+// Service Collection
+let providers: any[] = [
+];
+
+let declarations: any[] = [
+
+];
+
+// NgModule
+@NgModule({
+    providers: providers,
+    declarations: declarations,
+    exports: declarations
+})
+export class IvyAngularFullImageModule {
+}

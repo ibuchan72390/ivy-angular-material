cbcd88703144c225c5abceac2a03d6cab562988a
Ian Buchan <ibuchan72390@gmail.com>
2018-04-02 08:17:31 -0700

    Ensure we update this so that it ends with an exit code of 1 instead of 0.

--- test/test-proj.bash
+++ test/test-proj.bash
@@ -6,11 +6,8 @@ cd src/Ivy.Angular.$ProjName
 
 yarn install
 
-:: Need to be ensure this command is homogenized everywhere
-:: Perhaps we should homogenize everything to yarn test as well...
 yarn test
 
 cd $PWD
 
-:: This is required for us to ensure Bamboo acknowledges it as "passing"
-exit 0
+exit 1;

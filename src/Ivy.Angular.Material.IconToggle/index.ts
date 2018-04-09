204e425adaa2dbf1f9de363b9ebd777fddb52565
Ian Buchan <ibuchan72390@gmail.com>
2018-04-09 16:24:06 -0700

    Setup the placeholder for the IconToggle project. Need to relocate items from IAGE over to here.

---
+++ src/Ivy.Angular.Material.IconToggle/index.ts
@@ -0,0 +1,26 @@
+// Module
+export { IvyAngularMaterialIconToggleModule } from './ivy.angular.material.icon-toggle.module';
+
+
+
+// Components
+
+
+
+
+// Directives
+
+
+
+
+// Services
+
+
+
+
+// Pipes
+
+
+
+
+// Models
